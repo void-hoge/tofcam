1b67594f8eb66f4d9cc6c74a705044275ed9f2b4
void-hoge <muginoda@gmail.com>
2025-09-23 14:58:48 +0100

    iroiro

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,6 +1,8 @@
 #include <tofcam.hpp>
+#include <utility.hpp>
 #include <cstdlib>
 #include <cstdio>
+#include <vector>
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -8,11 +10,20 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
     const char* device = argv[1];
+
     auto camera = tofcam::Camera(device, 8);
+
+    const auto [width, height] = camera.get_size();
+    const auto [sizeimage, bytesperline] = camera.get_bytes();
+    std::vector<std::vector<uint16_t>> unpacked(4, std::vector<uint16_t>(width * height, 0));
+    
     camera.stream_on();
-    for (int i = 0; i < 120 * 5; i++) {
-        auto [bytesused, index] = camera.dequeue();
-        camera.enqueue(index);
+    for (int i = 0; i < 30 * 5; i++) {
+        for (int j = 0; j < 4; j++) {
+            const auto [data, index] = camera.dequeue();
+            tofcam::unpack_y12p(unpacked[j].data(), data, width, height, bytesperline);
+            camera.enqueue(index);
+        }
     }
     camera.stream_off();
 }
