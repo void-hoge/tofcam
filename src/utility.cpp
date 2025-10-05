4cc0648ba05e6f11945493c9c293f3464e6315f2
void-hoge <muginoda@gmail.com>
2025-10-06 00:32:33 +0100

    update depth calculation

--- src/utility.cpp
+++ src/utility.cpp
@@ -32,15 +32,16 @@ void compute_depth_amplitude(float *depth, float *amplitude, int16_t* raw,
     const float scale = bias / M_PI;
 
     for (size_t i = 0; i < num_pixels; i++) {
-        const auto I0 = static_cast<int16_t>(frame0[i]);
-        const auto I1 = static_cast<int16_t>(frame1[i]);
-        const auto I2 = static_cast<int16_t>(frame2[i]);
-        const auto I3 = static_cast<int16_t>(frame3[i]);
+        const int16_t I0 = frame0[i];
+        const int16_t I1 = frame1[i];
+        const int16_t I2 = frame2[i];
+        const int16_t I3 = frame3[i];
         const auto num = static_cast<int32_t>(I3 - I1);
         const auto den = static_cast<int32_t>(I0 - I2);
         raw[i] = I0 + I1 + I2 + I3;
         amplitude[i] = std::sqrt(den * den + num * num);
-        depth[i] = std::atan2(num, den) * scale + bias;
+        const float phase = std::atan2(num, den);
+        depth[i] = (phase >= M_PI || ((den == 0) && (num == 0))) ? 0.0f : phase * scale + bias;
     }
 }
 
