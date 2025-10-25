345033b0abb2f0591151a8129b6af1a3799b4c07
void-hoge <muginoda@gmail.com>
2025-10-25 15:34:20 +0100

    save frames later

--- examples/main.cpp
+++ examples/main.cpp
@@ -14,35 +14,41 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
     const char* device = argv[1];
+    constexpr uint32_t ITER = 60;
 
-    auto camera = tofcam::Camera(device, 8);
+    auto camera = tofcam::Camera(device, 8, tofcam::MemType::MMAP);
 
     const auto [width, height] = camera.get_size();
     const auto [sizeimage, bytesperline] = camera.get_bytes();
+
     std::vector unpacked(4, std::vector<int16_t>(width * height, 0));
-    std::vector<float> depth(width * height, 0.0f);
-    std::vector<float> amplitude(width * height, 0.0f);
-
-    std::ofstream depth_ofst("tofcam_depth.csv");
-    std::ofstream amplitude_ofst("tofcam_amplitude.csv");
-    std::ofstream ofst("tofcam.csv");
-
+    std::vector<std::vector<float>> depth;
+    std::vector<std::vector<float>> amplitude;
     camera.stream_on();
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < ITER; i++) {
         for (int j = 0; j < 4; j++) {
             const auto [data, index] = camera.dequeue();
             tofcam::unpack_y12p(unpacked[j].data(), data, width, height, bytesperline);
             camera.enqueue(index);
         }
-
+        depth.emplace_back(width * height, 0.0f);
+        amplitude.emplace_back(width * height, 0.0f);
         tofcam::compute_depth_confidence(
-                depth.data(), amplitude.data(), unpacked[0].data(), unpacked[1].data(), unpacked[2].data(), unpacked[3].data(),
+            depth.back().data(), amplitude.back().data(), unpacked[0].data(), unpacked[1].data(), unpacked[2].data(), unpacked[3].data(),
                 width * height, 75'000'000);
+    }
+    camera.stream_off();
+
+    fprintf(stderr, "saving frames\n");
+    std::ofstream depth_ofst("tofcam_depth.csv");
+    std::ofstream amplitude_ofst("tofcam_amplitude.csv");
+    std::ofstream ofst("tofcam.csv");
+    for (int i = 0; i < ITER; i++) {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                depth_ofst << depth[y * width + x];
-                amplitude_ofst << amplitude[y * width + x];
-                ofst << depth[y * width + x] << ", " << amplitude[y * width + x] << "\n";
+                depth_ofst << depth[i][y * width + x];
+                amplitude_ofst << amplitude[i][y * width + x];
+                ofst << depth[i][y * width + x] << ", " << amplitude[i][y * width + x] << "\n";
                 if (x < width - 1) {
                     depth_ofst << ", ";
                     amplitude_ofst << ", ";
@@ -52,5 +58,4 @@ int main(int argc, char* argv[]) {
             amplitude_ofst << "\n";
         }
     }
-    camera.stream_off();
 }
