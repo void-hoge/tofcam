89352dd6452ac214201d1f5e775b03a352c88ba8
void-hoge <muginoda@gmail.com>
2025-10-25 13:17:10 +0100

    dma buffer

--- include/buffpool.hpp
+++ include/buffpool.hpp
@@ -3,25 +3,40 @@
 #include <cstdint>
 #include <vector>
 #include <utility>
+#include <tuple>
 
 namespace tofcam {
 
 class BufferPool {
 public:
     virtual ~BufferPool() = default;
-    virtual void* sync_start(const uint32_t i) = 0;
-    virtual void sync_end(const uint32_t i) = 0;
+    virtual void* sync_start(const uint32_t index) = 0;
+    virtual void sync_end(const uint32_t index) = 0;
 };
 
 class MmapBufferPool final : public BufferPool {
 public:
     MmapBufferPool(const int fd, const uint32_t num_buffers);
 
-    void* sync_start(const uint32_t i) override;
-    void sync_end(const uint32_t i) override;
+    void* sync_start(const uint32_t index) override;
+    void sync_end(const uint32_t index) override;
 private:
     // addr, length
     std::vector<std::pair<void*, uint32_t>> buffers;
+};
+
+
+class DmaBufferPool final : public BufferPool {
+public:
+    DmaBufferPool(const char* allocator, const uint32_t num_buffers, const uint32_t length);
+
+    void* sync_start(const uint32_t index) override;
+    void sync_end(const uint32_t index) override;
+
+private:
+    int fd = -1;
+    // addr, length, fd
+    std::vector<std::tuple<void*, uint32_t, int>> buffers;
 };
 
 }
---
+++ src/buffpool.cpp
@@ -0,0 +1,76 @@
+#include <buffpool.hpp>
+#include <syscall.hpp>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <linux/dma-heap.h>
+#include <linux/dma-buf.h>
+#include <linux/videodev2.h>
+#include <system_error>
+
+namespace tofcam {
+
+MmapBufferPool::MmapBufferPool(const int fd, const uint32_t num_buffers) {
+    for (uint32_t i = 0; i < num_buffers; i++) {
+        struct v4l2_buffer buf = {};
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
+        if (syscall::ioctl(fd, VIDIOC_QUERYBUF, &buf) < 0) {
+            throw std::system_error(errno, std::generic_category(), "ioctl VIDIOC_QUERYBUF failed.");
+        }
+        auto addr = syscall::mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+        if (addr == MAP_FAILED) {
+            throw std::system_error(errno, std::generic_category(), "mmap failed.");
+        }
+        this->buffers.emplace_back(addr, buf.length);
+    }
+}
+
+void* MmapBufferPool::sync_start(const uint32_t index) {
+    return this->buffers[index].first;
+}
+
+void MmapBufferPool::sync_end(const uint32_t) {}
+
+DmaBufferPool::DmaBufferPool(const char* allocator, const uint32_t num_buffers, const uint32_t length) {
+    this->fd = syscall::open(allocator, O_RDWR | O_CLOEXEC, 0);
+    if (this->fd < 0) {
+        throw std::system_error(errno, std::generic_category(), "Failed to open a contiguous memory allocator.");
+    }
+    for (uint32_t i = 0; i < num_buffers; i++) {
+        struct dma_heap_allocation_data alloc = {};
+        alloc.len =  length;
+        alloc.fd = 0;
+        alloc.fd_flags = O_RDWR | O_CLOEXEC;
+        alloc.heap_flags = 0;
+        if (syscall::ioctl(this->fd, DMA_HEAP_IOCTL_ALLOC, &alloc) < 0) {
+            throw std::system_error(errno, std::generic_category(), "ioctl DMA_HEAP_IOCTL_ALLOC failed.");
+        }
+        auto addr = syscall::mmap(nullptr, length, PROT_READ | PROT_WRITE, MAP_SHARED, alloc.fd, 0);
+        if (addr == MAP_FAILED) {
+            throw std::system_error(errno, std::generic_category(), "mmap failed.");
+        }
+        this->buffers.emplace_back(addr, length, alloc.fd);
+    }
+}
+
+void* DmaBufferPool::sync_start(const uint32_t index) {
+    struct dma_buf_sync flags = {};
+    flags.flags = DMA_BUF_SYNC_START | DMA_BUF_SYNC_RW;
+    const auto& [addr, length, fd] = this->buffers[index];
+    if (syscall::ioctl(fd, DMA_BUF_IOCTL_SYNC, &flags) < 0) {
+        throw std::system_error(errno, std::generic_category(), "ioctl DMA_BUF_IOCTL_SYNC failed.");
+    }
+    return addr;
+}
+
+void DmaBufferPool::sync_end(const uint32_t index) {
+    struct dma_buf_sync flags = {};
+    flags.flags = DMA_BUF_SYNC_END | DMA_BUF_SYNC_RW;
+    const auto& [addr, length, fd] = this->buffers[index];
+    if (syscall::ioctl(fd, DMA_BUF_IOCTL_SYNC, &flags) < 0) {
+        throw std::system_error(errno, std::generic_category(), "ioctl DMA_BUF_IOCTL_SYNC failed.");
+    }
+}
+
+}
